d46cddf12fcaba0ef5326e5ef379d3f2a3f6b1fe
shorokhovkir <160240169+shorokhovkir@users.noreply.github.com>
2024-04-14 18:48:04 +0300

    Update tstack.h

--- include/tstack.h
+++ include/tstack.h
@@ -4,7 +4,39 @@
 
 template<typename T, int size>
 class TStack {
-  // добавьте код стека
+private:
+  T* ptr;
+  int z;
+public:
+  TStack() : z(0) {
+    ptr = new T[size];
+  }
+  void pushup(const T& c) {
+    if (size - 1 >= z) {
+      ptr[z++] = c;
+    }
+    else {
+      throw std::string("fall!");
+    }
+  }
+  T popback() {
+    if (z > 0) {
+      return ptr[--z];
+    }
+    else {
+      throw std::string("empty");
+    }
+  }
+  bool IfZero() const {
+    return z == 0;
+  }
+  T ElemUp() const {
+    if (z > 0) {
+      return ptr[z - 1];
+    }
+    else {
+      throw std::string("fall!");
+    }
+  }
 };
-
 #endif  // INCLUDE_TSTACK_H_
