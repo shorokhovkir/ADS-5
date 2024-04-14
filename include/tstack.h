1fd3e3cf0258825f44fdc3d02c067b20773a6ed2
shorokhovkir <160240169+shorokhovkir@users.noreply.github.com>
2024-04-14 20:27:14 +0300

    Update tstack.h

--- include/tstack.h
+++ include/tstack.h
@@ -1,4 +1,6 @@
 // Copyright 2021 NNTU-CS
+#include <iostream>
+#include <string>
 #ifndef INCLUDE_TSTACK_H_
 #define INCLUDE_TSTACK_H_
 
@@ -15,29 +17,26 @@ class TStack {
     void PushUp(const T& c) {
       if (size - 1 >= z) {
         ptr[z++] = c;
-      }
-      else {
-        throw std::string("fall!");
+      } else {
+       throw std::string("fall!");
       }
     }
     T PopBack() {
-      if (z > 0) {
-        return ptr[--z];
-      }
-      else {
-        throw std::string("empty");
-      }
+     if (z > 0) {
+      return ptr[--z];
+     } else {
+      throw std::string("empty");
+     }
     }
     bool IfZero() const {
-      return z == 0;
+     return z == 0;
     }
     T ElemUp() const {
-      if (z > 0) {
-        return ptr[z - 1];
-      }
-      else {
-        throw std::string("fall!");
-      }
+     if (z > 0) {
+      return ptr[z - 1];
+     } else {
+      throw std::string("fall!");
+     }
     }
 };
 #endif  // INCLUDE_TSTACK_H_
